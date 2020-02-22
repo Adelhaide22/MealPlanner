c4d582f6233425f90663b8d5f26044877bb3ddff
adelhaide <adelhaide22@gmail.com>
2020-02-22 21:18:24 +0200

    Add Recipe model and view

---
+++ MealPlanner/Models/Recipe.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MealPlanner.Models
+{
+    public class Recipe
+    {
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public List<string> Ingridients { get; set; }
+        public string Instruction { get; set; }
+    }
+}
---
+++ MealPlanner/Views/Recipe/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MealPlanner.Views
+{
+    public class Recipe : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
