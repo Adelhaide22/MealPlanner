bc85a626ae6b6084e5d80cc95c43aa3086986f0a
adelhaide <adelhaide22@gmail.com>
2020-02-23 21:19:02 +0200

    Add _RecipesLayout.cshtml

--- MealPlanner/Controllers/RecipeController.cs
+++ MealPlanner/Controllers/RecipeController.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using MealPlanner.Models;
+using MealPlanner.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MealPlanner.Controllers
@@ -9,8 +10,18 @@ namespace MealPlanner.Controllers
         // GET
         public IActionResult Index()
         {
-            var recipe = new Recipe() {Name = "Meat", Ingridients = new List<string>(){"Meat","Oil","Egg"}};
-            return View(recipe);
+            var recipeMeat = new Recipe() {Name = "Meat with egg", Ingridients = new List<string>(){"Meat","Oil","Egg"}};
+            var recipeSoup = new Recipe() {Name = "Tomato soup", Ingridients = new List<string>(){"Tomato","Water","Egg"}};
+            var menu = new List<CategoriesMenuViewModel>
+            {
+                new CategoriesMenuViewModel
+                 {
+                      CategoryName = "Meat",
+                      Recipes = new List<Recipe>() {recipeMeat, recipeSoup}
+                 }
+            };
+            ViewBag.Menu = menu;
+            return View(recipeMeat);
         }
     }
 }
